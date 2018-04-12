a2b3013960cb537fa673fddbd5e2fac45c602b88
rizretro <alxjacobs14@gmail.com>
2018-04-11 22:44:14 -0600

    Replaced player textures and added comments

--- MonoGameThing/Game1.cs
+++ MonoGameThing/Game1.cs
@@ -9,9 +9,9 @@ namespace MonoGameThing
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Texture2D textureBall;
-        Vector2 ballPosition;
-        float ballSpeed;
+        Texture2D playerTexture;
+        Vector2 playerPosition;
+        float playerSpeed;
 
         Texture2D bulletTexture;
         Vector2 bulletPosition;
@@ -28,18 +28,19 @@ namespace MonoGameThing
         {
             base.Initialize();
 
-            ballPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 100f;
+            playerPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            playerSpeed = 150f;
 
-            bulletPosition = ballPosition;
-            bulletSpeed = 500f;
+            bulletPosition = playerPosition;
+            bulletSpeed = 450f;
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            textureBall = Content.Load<Texture2D>("ball");
+            // Loads in textures
+            playerTexture = Content.Load<Texture2D>("player");
             bulletTexture = Content.Load<Texture2D>("bullet");
         }
 
@@ -54,18 +55,33 @@ namespace MonoGameThing
 
             var kstate = Keyboard.GetState();
 
+            // Player Movement
             if (kstate.IsKeyDown(Keys.W))
-                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                playerPosition.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kstate.IsKeyDown(Keys.S))
-                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                playerPosition.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kstate.IsKeyDown(Keys.A))
-                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                playerPosition.X -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kstate.IsKeyDown(Keys.D))
-                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            // Player screen wrap
+            if (playerPosition.X > graphics.PreferredBackBufferWidth)
+                playerPosition.X = 0;
+
+            if (playerPosition.X < 0)
+                playerPosition.X = graphics.PreferredBackBufferWidth;
+
+            if (playerPosition.Y > graphics.PreferredBackBufferHeight)
+                playerPosition.Y = 0;
+
+            if (playerPosition.Y < 0)
+                playerPosition.Y = graphics.PreferredBackBufferHeight;
+
+            // Bullet Movement
             if (kstate.IsKeyDown(Keys.Up))
                 bulletDirection = "UP";
 
@@ -78,12 +94,14 @@ namespace MonoGameThing
             if (kstate.IsKeyDown(Keys.Right))
                 bulletDirection = "RIGHT";
 
+            // Resets bullet if it has gone off screen
             if (bulletPosition.Y < 0 ||
                 bulletPosition.Y > graphics.PreferredBackBufferHeight ||
                 bulletPosition.X < 0 ||
                 bulletPosition.X > graphics.PreferredBackBufferWidth)
                 bulletDirection = "";
 
+            // Sets Bullet Direction
             switch (bulletDirection)
             {
                 case "UP":
@@ -99,7 +117,7 @@ namespace MonoGameThing
                     bulletPosition.X += bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
                     break;
                 default:
-                    bulletPosition = ballPosition;
+                    bulletPosition = playerPosition;
                     break;
             }
 
@@ -108,8 +126,9 @@ namespace MonoGameThing
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(Color.Black); // Frame Reset
 
+            // Draws Player and Bullet
             spriteBatch.Begin();
             spriteBatch.Draw(
                 bulletTexture,
@@ -123,30 +142,18 @@ namespace MonoGameThing
                 0f
             );
             spriteBatch.Draw(
-                textureBall,
-                ballPosition,
+                playerTexture,
+                playerPosition,
                 null,
                 Color.White,
                 0f,
-                new Vector2(textureBall.Width / 2, textureBall.Height / 2),
+                new Vector2(playerTexture.Width / 2, playerTexture.Height / 2),
                 Vector2.One,
                 SpriteEffects.None,
                 0f
             );
             spriteBatch.End();
-
-            if (ballPosition.X > graphics.PreferredBackBufferWidth)
-                ballPosition.X = 0;
-
-            if (ballPosition.X < 0)
-                ballPosition.X = graphics.PreferredBackBufferWidth;
-
-            if (ballPosition.Y > graphics.PreferredBackBufferHeight)
-                ballPosition.Y = 0;
-
-            if (ballPosition.Y < 0)
-                ballPosition.Y = graphics.PreferredBackBufferHeight;
-
+            
             base.Draw(gameTime);
         }
     }
