6e935cae6d2072569084588fee89c44effaa2b5b
rizretro <alxjacobs14@gmail.com>
2018-04-11 22:09:09 -0600

    Added bullet functionality

--- MonoGameThing/Game1.cs
+++ MonoGameThing/Game1.cs
@@ -8,9 +8,15 @@ namespace MonoGameThing
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
         Texture2D textureBall;
         Vector2 ballPosition;
         float ballSpeed;
+
+        Texture2D bulletTexture;
+        Vector2 bulletPosition;
+        float bulletSpeed;
+        string bulletDirection;
 
         public Game1()
         {
@@ -24,12 +30,17 @@ namespace MonoGameThing
 
             ballPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
             ballSpeed = 100f;
+
+            bulletPosition = ballPosition;
+            bulletSpeed = 500f;
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
             textureBall = Content.Load<Texture2D>("ball");
+            bulletTexture = Content.Load<Texture2D>("bullet");
         }
 
         protected override void UnloadContent()
@@ -56,16 +67,61 @@ namespace MonoGameThing
                 ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kstate.IsKeyDown(Keys.Up))
-                ballSpeed *= 1.01f;
+                bulletDirection = "UP";
+
+            if (kstate.IsKeyDown(Keys.Down))
+                bulletDirection = "DOWN";
+
+            if (kstate.IsKeyDown(Keys.Left))
+                bulletDirection = "LEFT";
+
+            if (kstate.IsKeyDown(Keys.Right))
+                bulletDirection = "RIGHT";
+
+            if (bulletPosition.Y < 0 ||
+                bulletPosition.Y > graphics.PreferredBackBufferHeight ||
+                bulletPosition.X < 0 ||
+                bulletPosition.X > graphics.PreferredBackBufferWidth)
+                bulletDirection = "";
+
+            switch (bulletDirection)
+            {
+                case "UP":
+                    bulletPosition.Y -= bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case "DOWN":
+                    bulletPosition.Y += bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case "LEFT":
+                    bulletPosition.X -= bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case "RIGHT":
+                    bulletPosition.X += bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                default:
+                    bulletPosition = ballPosition;
+                    break;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             spriteBatch.Begin();
+            spriteBatch.Draw(
+                bulletTexture,
+                bulletPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(bulletTexture.Width / 2, bulletTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
             spriteBatch.Draw(
                 textureBall,
                 ballPosition,
